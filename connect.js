532f631ff02f1b5213741f06b25dcdae07e4ed0c
Atsushi <atsushithesushi@gmail.com>
2021-04-29 09:44:47 -0300

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,6 +1,5 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 mongoose.connect("mongodb.com", { useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
-  
-if(err) return console.log("Erro ao conectar a DataBase Moongoose\n" + err) // Se der erro retornar no console
-})
+  if(err) return console.log("Error connecting to Mongoose DataBase\n" + err);
+});
