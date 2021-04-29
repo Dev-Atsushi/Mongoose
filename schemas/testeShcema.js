c720d00a37c15135881dcd64018ec2594fa7a34d
Atsushi <atsushithesushi@gmail.com>
2021-04-29 09:45:21 -0300

    Update testeShcema.js

--- schemas/testeShcema.js
+++ schemas/testeShcema.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose")
 
 const teste = mongoose.Schema({
   User: { type: String, required: true, default: "User" },
-  Idade: { type: Number, default: "Nenhum" },
-  E-mail: { type: String, default: "Nenhum" }
+  Age: { type: Number, default: "None" },
+  Email: { type: String, default: "None" }
 })
 
-const teste1 = mongoose.model("Teste", teste)
-exports.teste = teste1
+const teste1 = mongoose.model("Test", test)
+exports.teste = test
